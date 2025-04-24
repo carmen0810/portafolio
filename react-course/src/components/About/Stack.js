80a8715a7465534b6998f9fc451dca0866549a1b
carmen0810 <83844544+carmen0810@users.noreply.github.com>
2025-04-24 16:17:41 -0500

    push

--- react-course/src/components/About/Stack.js
+++ react-course/src/components/About/Stack.js
@@ -2,26 +2,24 @@ const Stack = () => {
     return (
       <>
         <h3 className="title-font white-text ">
-        Estas son algunas de las herramientas y tecnologías con las que he estado trabajando:
+        Estas son las herramientas y tecnologías con las que he estado trabajando:
         </h3>
         <ol className="stack-section ">
           <li>React.js</li>
-          <li>Bootstrap</li>
+          <li>React Native</li>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
           <li>HTML5</li>
           <li>CSS3</li>
-          <li>JavaScript ES6+</li>
           <li>Firebase</li>
           <li>Node.js</li>
           <li>NPM</li>
+          <li>WordPress</li>
+          <li>Bootstrap</li>
           <li>Github</li>
           <li>Git</li>
           <li>Figma</li>
           <li>Visual Studio Code</li>
-          <li>Adobe Ilustrador</li>
-          <li>Zeplin</li>
-          <li>Marvel App</li>
-          <li>UserTesting</li>
-          <li>Adobe Photoshop</li>
         </ol>
       </>
     );
--- react-course/src/components/data/ProjectsData.js
+++ react-course/src/components/data/ProjectsData.js
@@ -1,65 +1,64 @@
 import petWorld from "../../images/Red-social-petWorld.jpg";
 import PokeMatch from "../../images/PokeMatch.jpg";
 import mdlinks from "../../images/md-links.jpg";
-import Cargill from "../../images/cargill.jpg"
-
+import Cargill from "../../images/cargill.jpg";
+import PlataformaEducativa from "../../images/Pizarra21.jpg"
 import GestorAdmin from "../../images/GestorAdministratvo.jpeg";
 import DetectorDNI from "../../images/imagen2.jpeg";
 import Gym from "../../images/imagen3.jpeg";
 import Finanzas from "../../images/imagen4.jpeg";
 import Votacion from "../../images/imagen5.jpeg";
 import Tienda from "../../images/muomarket.jpeg";
-import RecursosHumanos from "../../images/RRHH.jpeg";
 import CasadeCambio from "../../images/tucambiototal.jpeg";
 
 const ProjectsPreviewData = [
   {
     id: 1,
     img: Cargill,
-    name: "Cargill-qrs",
-    stack: ["< React.js />", "< CSS3 />", "< HTML5 />", "< Firebase />","< Git />","< GitHub />","< npm />","< Boostrap />"],
+    name: "Cargill",
+    stack: ["< React.js />", "< Firebase />", "< Boostrap />", "< Figma />", "< GitHub />"],
     description:
-      "Cargill-qrs es una single page application (SPA) desarrollado usando React para la empresa Cargill. Nuestra aplicación es una interfaz con estado sincrónico para la gestión de las quejas, reclamos y seguimiento de las mismas por Servicio al Cliente.",
+      "Proyecto que trabajamos en grupo desde cero en una Hackathon Talent Fest de Laboratoria junto con la empresa Cargill, se desarrolló una aplicación para la gestión de las quejas, reclamos y seguimiento de las mismas por Servicio al Cliente usando React.js.",
   },
   {
     id: 2,
-    img: petWorld,
-    name: "Red Social petWorld",
-    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />", "< Firebase />","< Git />","< GitHub />"],
+    img: GestorAdmin,
+    name: "Sistema de Recursos Humanos",
+    stack: ["< React JS />", "< React Native />"],
     description:
-      "PetWorld es una red social dirigida a usuarios que tienen mascotas, esta en modo responsivo.",
+      "Desarrollé la plataforma web y app usando React JS y React Native.",
   },
   {
     id: 3,
-    img: PokeMatch,
-    name: "Juego de memoria PokeMach",
-    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />", "< Git />", "< GitHub />"],
+    img: Votacion,
+    name: "Sistema de votación de partidos políticos",
+    stack: ["< React JS />", "< Bootstrap 5 />", "< Figma />"],
     description:
-      "Es un juego de memoria con cartas, solo tendrás que voltearlas y hacer match.",
+      "PWA desarrollada con React JS, Bootstrap 5 y diseño de UI en Figma.",
   },
   {
     id: 4,
-    img: mdlinks,
-    name: "md-links-Carmen",
-    stack: ["< Node.js />", "< NPM />", "< Git />", "< GitHub />"],
+    img: CasadeCambio,
+    name: "Plataforma de casa de cambio",
+    stack: ["< React JS />", "< Bootstrap 4 />", "< Figma />"],
     description:
-      "Es una librería que permite validar URLs dentro de archivos markdown. Te permitirá obtener: estado de los enlaces, enlaces totales, enlaces únicos y enlaces rotos.",
+      "Se implementó con React JS y Bootstrap 4, UI diseñada en Figma.",
   },
   {
     id: 5,
-    img: GestorAdmin,
-    name: "Gestor administrativo",
-    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />"],
+    img: PlataformaEducativa,
+    name: "Plataforma educativa",
+    stack: ["< VueJS />", "< Vuetify />", "< Firebase />"],
     description:
-      "Programe el front-end para una plataforma de gestor de actividades para los colaboradores.",
+      "Desarrollo frontend en VueJS y Vuetify, con Firebase como backend.",
   },
   {
     id: 6,
-    img: DetectorDNI,
-    name: "Detector de documentos DNI y carnet extranjería",
-    stack: ["< Marvel App />"],
+    img: petWorld,
+    name: "Red Social petWorld",
+    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />", "< Firebase />", "< Figma />", "< Git />","< GitHub />"],
     description:
-      "Diseñe la UI del flujo de escanear documento de identidad.",
+      "Proyecto que trabajamos en grupo desde cero en Laboratoria llamado PetWorld, es una red social dirigida a usuarios que tienen mascotas.",
   },
 ];
 
@@ -67,98 +66,99 @@ const ProjectsData = [
   {
     id: 1,
     img: Cargill,
-    name: "Cargill-qrs",
-    stack: ["< React.js />", "< CSS3 />", "< HTML5 />", "< Firebase />","< Git />","< GitHub />","< npm />","< Boostrap />"],
+    name: "Cargill",
+    stack: ["< React.js />", "< Firebase />", "< Boostrap />", "< Figma />", "< GitHub />"],
     description:
-      "Cargill-qrs es una single page application (SPA) desarrollado usando React para la empresa Cargill. Nuestra aplicación es una interfaz con estado sincrónico para la gestión de las quejas, reclamos y seguimiento de las mismas por Servicio al Cliente.",
+      "Proyecto que trabajamos en grupo desde cero en una Hackathon Talent Fest de Laboratoria junto con la empresa Cargill, se desarrolló una aplicación para la gestión de las quejas, reclamos y seguimiento de las mismas por Servicio al Cliente usando React.js.",
   },
+  
   {
     id: 2,
-    img: petWorld,
-    name: "Red Social petWorld",
-    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />", "< Firebase />","< Git />","< GitHub />"],
+    img: GestorAdmin,
+    name: "Sistema de Recursos Humanos",
+    stack: ["< React JS />", "< React Native />"],
     description:
-      "PetWorld es una red social dirigida a usuarios que tienen mascotas, esta en modo responsivo.",
+      "Desarrollé la plataforma web y app usando React JS y React Native.",
   },
   {
     id: 3,
-    img: PokeMatch,
-    name: "Juego de memoria PokeMach",
-    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />", "< Git />", "< GitHub />"],
+    img: Votacion,
+    name: "Sistema de votación de partidos políticos",
+    stack: ["< React JS />", "< Bootstrap 5 />", "< Figma />"],
     description:
-      "Es un juego de memoria con cartas, solo tendrás que voltearlas y hacer match.",
+      "PWA desarrollada con React JS, Bootstrap 5 y diseño de UI en Figma.",
   },
   {
     id: 4,
-    img: mdlinks,
-    name: "md-links-Carmen",
-    stack: ["< Node.js />", "< NPM />", "< Git />", "< GitHub />"],
+    img: CasadeCambio,
+    name: "Plataforma de casa de cambio",
+    stack: ["< React JS />", "< Bootstrap 4 />", "< Figma />"],
     description:
-      "Es una librería que permite validar URLs dentro de archivos markdown. Te permitirá obtener: estado de los enlaces, enlaces totales, enlaces únicos y enlaces rotos.",
+      "Se implementó con React JS y Bootstrap 4, UI diseñada en Figma.",
   },
   {
     id: 5,
-    img: GestorAdmin,
-    name: "Gestor administrativo",
-    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />"],
+    img: PlataformaEducativa,
+    name: "Plataforma educativa",
+    stack: ["< VueJS />", "< Vuetify />", "< Firebase />"],
     description:
-      "Programe el front-end para una plataforma de gestor de actividades para los colaboradores.",
+      "Desarrollo frontend en VueJS y Vuetify, con Firebase como backend.",
   },
   {
     id: 6,
-    img: DetectorDNI,
-    name: "Detector de documentos DNI y carnet extranjería",
-    stack: ["< Marvel App />"],
+    img: petWorld,
+    name: "Red Social petWorld",
+    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />", "< Firebase />", "< Figma />", "< Git />","< GitHub />"],
     description:
-      "Diseñe la UI del flujo de escanear documento de identidad.",
+      "Proyecto que trabajamos en grupo desde cero en Laboratoria llamado PetWorld, es una red social dirigida a usuarios que tienen mascotas.",
   },
   {
     id: 7,
+    img: DetectorDNI,
+    name: "Detector de documentos DNI y carnet extranjería",
+    stack: ["< Figma />"],
+    description:
+      "Diseñe la UI de manera funcional, intuitiva y adaptada al usuario final",
+  },
+  {
+    id: 8,
     img: Gym,
     name: "Software para gimnasios",
     stack: ["< Figma />"],
     description:
-      "Diseñe la UI para un plataforma de Gimnasios",
-  },
-  {
-    id: 8,
-    img: Finanzas,
-    name: "Software para la gestión de finanzas",
-    stack: ["< Zeplin />"],
-    description:
-      "Diseñe la UI de la plataforma de finanzas personales.",
+      "Diseñe la UI de la plataforma de manera funcional, intuitiva y adaptada al usuario final",
   },
   {
     id: 9,
-    img: Votacion,
-    name: "Sistema de votación de partidos políticos",
+    img: Finanzas,
+    name: "Software para la gestión de finanzas",
     stack: ["< Figma />"],
     description:
-      "Diseñe la UI del sistema para un partido político",
+      "Diseñe la UI de la plataforma de manera funcional, intuitiva y adaptada al usuario final",
   },
   {
     id: 10,
     img: Tienda,
     name: "Plataforma de compra y venta al por menor",
-    stack: ["< UserTesting />", "< Figma />"],
+    stack: ["< Figma />"],
     description:
-      "Implemente la experiencia de usuario de todo el proceso de compra, venta y delivery",
+      "Diseñe la UI de la plataforma de manera funcional, intuitiva y adaptada al usuario final",
   },
   {
     id: 11,
-    img: RecursosHumanos,
-    name: "Software de recursos humanos",
-    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />"],
+    img: PokeMatch,
+    name: "Juego de memoria PokeMach",
+    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />", "< Git />", "< GitHub />", "< Figma />"],
     description:
-      "Realice el front-end del sistema"
+      "Proyecto que trabajamos en grupo desde cero en Laboratoria, desarrollamos un juego de memoria con cartas.",
   },
   {
     id: 12,
-    img: CasadeCambio,
-    name: "Plataforma de casa de cambio",
-    stack: ["< JavaScript />", "< CSS3 />", "< HTML5 />", "< Figma />"],
+    img: mdlinks,
+    name: "md-links-Carmen",
+    stack: ["< Node.js />", "< NPM />", "< Git />", "< GitHub />"],
     description:
-      "Diseñe la UI y programe el front-end de la página web.",
+      "Proyecto de Laboratoria donde se realizó una librería que permite validar URLs dentro de archivos markdown. Te permitirá obtener: estado de los enlaces, enlaces totales, enlaces únicos y enlaces rotos.",
   },
 ];
 
